556a0012d84cba6683b371bda37a1d962f5f0164
Samadeen <samadmomoh@gmail.com>
2022-10-13 02:54:58 -0700

    Betty Style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Description: program prints
  * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
