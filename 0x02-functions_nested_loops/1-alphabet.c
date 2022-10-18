a893bb54c0cf12f0c4f1220ee1b55843018dd11b
Samadeen <samadmomoh@gmail.com>
2022-10-18 11:07:06 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,20 +5,14 @@
  *
  * Return: Always 0.
  */
-
-
 void print_alphabet(void)
 {
     char ch;
-
     ch = 'a'
-
     while (ch <= 'z')
     {
         _putchar(ch)
         ch++;
     }
-
     _putchar('\n');
-    
 }
